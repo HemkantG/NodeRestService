bb85da5e80040137ef3d4e4cb13f963af1474391
HemkantG <gangurdehemkant@gmail.com>
2019-10-14 13:31:54 +0530

    Updated the api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req, res)=>{
-    res.send('Hello from api updated')
+    res.send('Hello from api updated again')
 })
 
 app.listen(3000,()=>{
